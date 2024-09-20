19e9172492eb8d1317b6a70a7a79fb2bd34eb4fe
marek-novotny-st <122443470+marek-novotny-st@users.noreply.github.com>
2024-09-20 14:06:03 +0200

    Clean main.py

--- src/main.py
+++ src/main.py
@@ -5,13 +5,10 @@ Created on Thu Feb 15 12:45:09 2024
 @author: marek novotny
 """
 from src.product_scripts import ips8200hq
-from src.instrument_drivers.daq import DAQ
-from src.instrument_drivers.InstrumentDiscovery import InstrumentDiscovery
 
 """
 Usage example:
     ips8200hq.ips8200hq_out16a1_pgood_convex_ramp()
 Please keep your custom main code on your private branches
 """
-with DAQ(InstrumentDiscovery()) as meas:
-    meas(DAQ.Mode.Power, DAQ.Params((0, 45),(0, 0.5)))
+pass
